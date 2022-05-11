aa50afa5371a26d2d87589dc9fe4974f36d7a466
stolomeo <tolomeosebastian@gmail.com>
2022-05-11 00:00:22 -0400

    Add title and instructions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <h1 className="title">Tenzi</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls
+      </p>
+    </main>
+  );
 }
