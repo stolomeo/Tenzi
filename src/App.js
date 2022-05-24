9c485f0016366a71ac202b5fad93a5a6b8a8b7e1
stolomeo <tolomeosebastian@gmail.com>
2022-05-23 22:21:53 -0400

    Fix nested elements

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Confetti from "react-confetti";
 import Main from "./components/Main";
-import "./scss/App.scss";
+import styled from "styled-components";
 
 export default function App() {
   const [tenzi, setTenzi] = useState(false);
 
   return (
-    <main>
+    <Wrapper>
       {tenzi && <Confetti />}
       <Header />
       <Main tenzi={tenzi} setTenzi={setTenzi} />
-    </main>
+    </Wrapper>
   );
 }
+const Wrapper = styled.div`
+  background-color: #ffffff;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 0.5rem;
+  width: 75vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+  padding: 2rem;
+`;
