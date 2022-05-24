93e99a7441eaffd6959d77a2401db12fb3d6f869
stolomeo <tolomeosebastian@gmail.com>
2022-05-23 22:41:15 -0400

    Change styles

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ export default function App() {
 }
 const Wrapper = styled.div`
   background-color: #ffffff;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;
   border-radius: 0.5rem;
   width: 75vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 40px;
-  padding: 2rem;
+  gap: 50px;
+  padding: 2.5rem;
 `;
